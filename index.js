db92f3f6811682bda7166a7701c5935d93886498
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-10-21 11:36:08 +0200

    Explorer main page

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const httpPort = process.env.HTTP_PORT || 3000
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('static'))
 
 app.listen(httpPort, function () {
   console.log(`Example app listening on port ${httpPort}!`)
