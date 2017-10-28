0ab54320547c7d6fd4e7e446e2afb4fe771192b4
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-10-28 16:16:49 +0200

    Data - objects by type

--- src/data.js
+++ src/data.js
@@ -1,8 +1,14 @@
 const angular = require('angular')
+const _ = require('lodash')
 angular.module('yellow-explorer')
 
 .factory('dataService',($http)=>{
-  return $http.get('/data').then((response)=> response.data)
+  return $http.get('/data').then((response)=> {    
+    const data = response.data
+    data.byType = _.groupBy(data,'type')
+    return data
+  }
+  )
 })
 
 .controller('data',($scope,dataService)=>{
