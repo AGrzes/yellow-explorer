1f00e1d091b1d65b90281784914a3101b505e996
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-25 23:00:07 +0100

    Migrated metadata to axios client

--- src/metadata.js
+++ src/metadata.js
@@ -1,14 +1,4 @@
-const angular = require('angular')
+const axios = require('axios')
 const _ = require('lodash')
 const metadata = require('yellow/src/metadata/main')
-angular.module('yellow-explorer')
-
-.factory('metadataService',($http)=>{
-  return $http.get('/metadata').then((response) => new metadata.Metadata(response.data))
-})
-
-.controller('metadata',($scope,metadataService)=>{
-  metadataService.then((metadata)=>{
-    $scope.metadata = metadata
-  })
-})
+module.exports = axios.get('/metadata').then((response) => new metadata.Metadata(response.data))
