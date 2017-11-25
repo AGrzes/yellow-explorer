b1b6fd2ae2476334d7d2154f34adc4a838e831f1
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-25 22:57:32 +0100

    Migrated config to axios client

--- src/config.js
+++ src/config.js
@@ -1,12 +1,2 @@
-const angular = require('angular')
-angular.module('yellow-explorer')
-
-.factory('configService',($http)=>{
-  return $http.get('/config').then((response)=> response.data)
-})
-
-.controller('config',($scope,configService)=>{
-  configService.then((config)=>{
-    $scope.config = config
-  })
-})
+const axios = require('axios')
+module.exports = axios.get('/config').then((response) => response.data)
