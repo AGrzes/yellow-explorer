3a0ca53e3eeeccadf9edfb3f8fba7faaa6b00fa4
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-30 22:53:55 +0100

    Generating item list

--- src/components/list.js
+++ src/components/list.js
@@ -7,23 +7,36 @@ const determineWrapper = (style) => {
 const determineItemWrapper = (style) => {
   return style == 'list' ? 'li' : 'div'
 }
-Vue.component('yellow-list', {
-  render: function (createElement) {
-    const wrapper = determineWrapper(this.config.style)
-    const itemWrapper = `yellow-${_.kebabCase(this.config.item.type)}`
-    return createElement(
-      wrapper,_.map(this.items,(item)=>createElement(itemWrapper,{
-        props: {
-          config: this.config.item,
-          data: item
-        }
-      }))
-    )
-  },
-  props: {
-    config: {
-      type: Object,
-      required: true
+dataPromise.then((data)=>{
+  Vue.component('yellow-list', {
+    render: function (createElement) {
+      const wrapper = determineWrapper(this.config.style)
+      const itemWrapper = `yellow-${_.kebabCase(this.config.item.type)}`
+      return createElement(
+        wrapper,_.map(this.items,(item)=>createElement(itemWrapper,{
+          props: {
+            config: this.config.item,
+            data: item
+          }
+        }))
+      )
+    },
+    data(){
+      let items
+      if (this.config.list.selector){
+        const filter = new Function('any','return '+this.config.list.selector.replace(/this/g,'any'))
+        items = _.filter(data.model,filter)
+      }
+      return {
+        items:items
+      }
+    },
+    props: {
+      config: {
+        type: Object,
+        required: true
+      }
     }
-  }
+  })
 })
+
