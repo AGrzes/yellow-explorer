43de7be26054a2658d4576fa84ae9f4606078ce3
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-30 22:00:04 +0100

    Determining wrapper style

--- src/components/list.js
+++ src/components/list.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import _ from 'lodash'
+import dataPromise from '../data'
 Vue.component('yellow-list', {
   render: function (createElement) {
+    const wrapper = this.config.style == 'list' ? 'ul' : 'div'
     return createElement(
-      'b','list'
+      wrapper,'list'
     )
   },
   props: {
