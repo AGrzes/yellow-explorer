6ee79ef7b5594747f328770bba315f4e94455db0
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-30 22:12:40 +0100

    Creating item wrappers

--- src/components/list.js
+++ src/components/list.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import _ from 'lodash'
 import dataPromise from '../data'
 const determineWrapper = (style) => {
-  return this.config.style == 'list' ? 'ul' : 'div'
+  return style == 'list' ? 'ul' : 'div'
+}
+const determineItemWrapper = (style) => {
+  return style == 'list' ? 'li' : 'div'
 }
 Vue.component('yellow-list', {
   render: function (createElement) {
     const wrapper = determineWrapper(this.config.style)
+    const itemWrapper = determineItemWrapper(this.config.style)
     return createElement(
-      wrapper,'list'
+      wrapper,_.map(this.items,(item)=>createElement(itemWrapper,item))
     )
   },
   props: {
