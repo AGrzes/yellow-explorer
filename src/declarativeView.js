4b8e72b4a353a4d0cc3a9e750985536dad578096
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-30 21:35:16 +0100

    Passing arguments to component

--- src/declarativeView.js
+++ src/declarativeView.js
@@ -3,8 +3,11 @@ import _ from 'lodash'
 Vue.component('yellow-declarative-view', {
   render: function (createElement) {
     return createElement(
-      `yellow-${_.kebabCase(this.config.contents[0].type)}`,   // tag name
-      this.$slots.default // array of children
+      `yellow-${_.kebabCase(this.config.contents[0].type)}`, {
+        props: {
+          config: this.config.contents[0]
+        }
+      }
     )
   },
   props: {
