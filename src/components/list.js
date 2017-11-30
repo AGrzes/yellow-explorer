281fc8aaa25706f2b42b0992c61483dfb0ab12c0
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-30 22:02:08 +0100

    Moved wrapper determinstion to separate function

--- src/components/list.js
+++ src/components/list.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import _ from 'lodash'
 import dataPromise from '../data'
+const determineWrapper = (style) => {
+  return this.config.style == 'list' ? 'ul' : 'div'
+}
 Vue.component('yellow-list', {
   render: function (createElement) {
-    const wrapper = this.config.style == 'list' ? 'ul' : 'div'
+    const wrapper = determineWrapper(this.config.style)
     return createElement(
       wrapper,'list'
     )
