09b1515bd37c4408b1262eb30bbafc6e0204fbf7
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-30 21:30:08 +0100

    Changed component naming scheme

--- src/declarativeView.js
+++ src/declarativeView.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import _ from 'lodash'
-Vue.component('declarative-view', {
+Vue.component('yellow-declarative-view', {
   render: function (createElement) {
     return createElement(
-      `_.kebabCase(this.config.contents[0].type)-control`,   // tag name
+      `yellow-${_.kebabCase(this.config.contents[0].type)}`,   // tag name
       this.$slots.default // array of children
     )
   },
