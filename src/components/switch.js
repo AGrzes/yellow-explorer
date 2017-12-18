8eee1906f4d62bc946ac8408edf8d79bddb5f873
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-12-18 22:30:33 +0100

    Switch implementation

--- src/components/switch.js
+++ src/components/switch.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import _ from 'lodash'
 Vue.component('yellow-switch', {
   render: function (createElement) {
-    return createElement(
-      `span`
-    )
+    const key = this.data[this.config.case.field]
+    const caseConfig = this.config.when[key] || this.config.when['default']
+    if (caseConfig){
+      return createElement(
+        `yellow-${_.kebabCase(caseConfig.type)}`,{props: {
+          config: caseConfig,
+          data: this.data
+        }}
+      )
+    }
+    
   },
   props: {
     config: {
