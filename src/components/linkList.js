994ecb9cf10a19fac80ec55d01be6aaeeaed7b0c
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-12-26 20:42:22 +0100

    Expression for formatting

--- src/components/linkList.js
+++ src/components/linkList.js
@@ -1,25 +1,35 @@
 import Vue from 'vue'
 import _ from 'lodash'
 import dataPromise from '../data'
-
+import Handlebars from 'handlebars'
 Vue.component('yellow-link-list', {
   render: function (createElement) {
     const wrapper = 'ul'
     const itemWrapper = 'li'
     return createElement(
-      wrapper,_.map(this.items,(item)=>createElement(itemWrapper,{
-        
-      },item[this.config.label.field]))
+      wrapper, _.map(this.items, (item) =>
+        createElement(itemWrapper, {
+          domProps: {
+            innerHTML: this.expression(item)
+          }
+        }))
     )
   },
-  data(){
-    let items
-    if (this.config.data.constructor){
-      const constructor = new Function('return '+this.config.data.constructor)
+  data() {
+    let items, expression
+    if (this.config.data.constructor) {
+      const constructor = new Function('return ' + this.config.data.constructor)
       items = constructor.apply(this.data)
     }
+    if (this.config.label.field) {
+      expression = Handlebars.compile(`{{${this.config.label.field}}}`)
+    }
+    if (this.config.label.expression) {
+      expression = Handlebars.compile(this.config.label.expression)
+    }
     return {
-      items:items
+      items,
+      expression
     }
   },
   props: {
@@ -30,6 +40,6 @@ Vue.component('yellow-link-list', {
     data: {
       type: Object,
       required: true
-    },   
+    },
   }
 })
